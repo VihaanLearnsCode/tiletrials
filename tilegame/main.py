36d5fd2edd7756b4730ccf4fbb43b396c352f141
VihaanLearnsCode <vihaanrawarkhare@gmail.com>
2025-11-11 10:57:23 -0600

    added progress on updating the board

--- tilegame/main.py
+++ tilegame/main.py
@@ -1,18 +1,30 @@
-from .state import update_board, check_end
+from .state import update_board, check_end, validSequence
 from .create import generate
 from .stats import maxTile, statistics
 
+
 def simulate(moves: str):
+    
     board = generate()
 
-    while(True): #loop until game over
+    i = 0
+    best_tile = 0
+    best_coods = (-1, -1)
+    if not validSequence(moves):
+        return moves, best_tile, best_coods, board
+    
+    while(i < 1000): #loop until 2048 is found or 1000 moves have been played or 'game over'
         for move in moves:
             best_tile, best_coods = maxTile(board)
-            if check_end(board, move) or best_tile == 2048:
+            if check_end(board, move) or best_tile >= 2048:
                 return moves, best_tile, best_coods, board
             update_board(move)
+        i += 1
+
+    return moves, best_tile, best_coods, board
 
 def main():
     result = simulate() #result = (moves, best_tile, best_coods, board)
+    print(f"Best Tile Found{result[0]}")
     statistics(*result)
     return 1
--- tilegame/state.py
+++ tilegame/state.py
@@ -1,12 +1,40 @@
 import numpy as np
 
 def update_board(board, move) -> bool:
-    #try move
-    new_board = board.copy()
-    for row in new_board:
-        #do something based on direction
-        print("remove this line")
-    return (new_board == board) #no change or change?
+    #decide transform based on move
+    if move == 'A':
+        board = np.rot90(board)
+    elif move == 'S':
+        board = np.rot90(np.rot90(board))
+    elif move == 'D':
+        board = np.rot90(board, k=-1)
+
+    #moves up
+    for row in range(1, 4):
+        for col in range(4):
+            
+            #merges
+            if board[row][col] == board[row - 1][col]:
+                board[row - 1][col] = board[row][col] * 2
+                board[row][col] = 0
+
+            #compresses up
+            if board[row - 1][col] == 0:
+                board[row - 1][col] = board[row][col]
+                board[row][col] = 0
+            
+            
+
+
+    #transforms back into original
+    if move == 'A':
+        board = np.rot90(board, k=-1)
+    elif move == 'S':
+        board = np.rot90(np.rot90(board))
+    elif move == 'D':
+        board = np.rot90(board)
+
+    return board #no change or change?
 
 def try_move(board, move):
     return (True, board)
@@ -20,12 +48,12 @@ def spawn_tile(board):
     weights = [0.9, 0.1]
 
     empty = np.argwhere(board == 0)
-
-    print(empty)
     if empty.size == 0:
         return board
     
     i, j = empty[np.random.randint(0, len(empty))]
     board[i, j] = np.random.choice(vals, p=weights)
-    
     return board #returns same board if no space available
+
+def validSequence(moves: str):
+    return set(moves) not in ('W', 'A', 'S', 'D') or len(moves) > 10
