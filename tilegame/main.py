a2c52b04fd96c582d59a34264906fe412736df30
VihaanLearnsCode <vihaanrawarkhare@gmail.com>
2025-11-11 03:38:37 -0600

    updated main

--- tilegame/main.py
+++ tilegame/main.py
@@ -4,11 +4,13 @@ from .stats import maxTile, statistics
 
 def simulate(moves: str):
     board = generate()
-    for move in moves:
-        best_tile, best_coods = maxTile(board)
-        if check_end(board, move) or best_tile == 2048:
-            return moves, best_tile, best_coods, board
-        update_board(move)
+
+    while(True): #loop until game over
+        for move in moves:
+            best_tile, best_coods = maxTile(board)
+            if check_end(board, move) or best_tile == 2048:
+                return moves, best_tile, best_coods, board
+            update_board(move)
 
 def main():
     result = simulate() #result = (moves, best_tile, best_coods, board)
